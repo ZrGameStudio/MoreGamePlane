1cc203bfee8340705f9a07ceae35fae1a7b4001c
ZrGameStudio <122069480+ZrGameStudio@users.noreply.github.com>
2023-01-09 18:02:23 +0800

    Add files via upload

---
+++ Release/MoreGame&Share/build_html_android66.bat
@@ -0,0 +1,3 @@
+.\bin\FeaturedGamesPageGenerator.exe .\dist_android66\more_game_config.json .\dist_android66\more_game\index.html
+.\bin\InvitePageGenerator.exe .\dist_android66\share_config.json .\dist_android66\share_game\index.html
+pause
