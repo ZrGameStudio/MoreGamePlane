01d274f19b173785433dc6ebbf2a72e64891f735
ZrGameStudio <122069480+ZrGameStudio@users.noreply.github.com>
2023-01-09 18:11:13 +0800

    Delete Release/MoreGame&Share directory

--- Release/MoreGame&Share/build_html_android66.bat
+++
@@ -1,3 +0,0 @@
-.\bin\FeaturedGamesPageGenerator.exe .\dist_android66\more_game_config.json .\dist_android66\more_game\index.html
-.\bin\InvitePageGenerator.exe .\dist_android66\share_config.json .\dist_android66\share_game\index.html
-pause
--- Release/MoreGame&Share/build_html_ios.bat
+++
@@ -1,3 +0,0 @@
-.\bin\FeaturedGamesPageGenerator.exe .\dist_ios\more_game_config.json .\dist_ios\more_game\index.html
-.\bin\InvitePageGenerator.exe .\dist_ios\share_config.json .\dist_ios\share_game\index.html
-pause
